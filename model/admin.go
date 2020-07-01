e551800314949398ce5134033f2212615a612dcf
AnyDev <794763733@qq.com>
2020-07-01 15:46:04 +0800

    add static file service.

--- model/admin.go
+++ model/admin.go
@@ -1,12 +1,12 @@
 package model
 
 type Admin struct {
-	ID        uint   `database:"primary_key" json:"id"`
-	Name      string `database:"unique_index" json:"name"`
-	Password  string `database:"not null;char(60)" json:"password"`
-	Introduce string `database:"varchar(50)" json:"introduce"`
-	Picture   string `database:"not null;varchar(255)" json:"picture"`
-	CreatedAt string `database:"not null;datetime" json:"picture"`
+	ID        uint   `gorm:"primary_key" json:"id"`
+	Name      string `gorm:"unique_index" json:"name"`
+	Password  string `gorm:"not null;char(60)" json:"password"`
+	Introduce string `gorm:"varchar(50)" json:"introduce"`
+	Picture   string `gorm:"not null;varchar(255)" json:"picture"`
+	CreatedAt string `gorm:"not null;datetime" json:"picture"`
 }
 
 func (admin *Admin) TableName() string {
--- route/route.go
+++ route/route.go
@@ -3,6 +3,9 @@ package route
 import "github.com/gin-gonic/gin"
 
 func Route(engine *gin.Engine) {
+	// 静态文件服务
+	engine.Static("/static", "./static")
+	// 路由
 	engine.GET("/test", func(context *gin.Context) {
 		context.JSON(200, gin.H{
 			"ok":  true,
